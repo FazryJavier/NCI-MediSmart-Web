b78272f981cc90e3f34ff368bc86c62e06975a3b
Fazry Javier Nugraha <fazryjavier125@gmail.com>
2023-08-22 01:53:58 +0700

    fix blog

--- resources/views/UserPage/Pages/blog.blade.php
+++ resources/views/UserPage/Pages/blog.blade.php
@@ -23,17 +23,21 @@
     <section class="section-blog">
         <div class="blog">
             <div class="container">
-                <div class="content">
-                    <div class="text">
-                        <h1>NCIMedismart/Blog</h1>
-                    </div>
-                    <div class="top-card">
-                        <img src="{{ asset('assets/img/blog.png') }}" alt="">
+                <div class="text">
+                    <h1>NCIMedismart/Blog</h1>
+                </div>
+
+                <div class="top-card">
+                    <div class="content">
+                        <div class="image">
+                            <img src="{{ asset('assets/img/blog.png') }}" alt="">
+                        </div>
                         <div class="description">
                             <h3>Pengalaman Kami menggunakan SIM-RS NCI MediSmart</h3>
-                            <p>Manfaat yang saya rasakan sebagai Direktur Rumah Sakit Al-Islam Bandung dengan penggunaan
-                                SIMRS NCI-MediSmart diantaranya adalah kecepatan dalam pengambilan keputusan, akurasi
-                                dalam mengidentifikasi masalah, kemudahan menyusun strategi...</p>
+                            <p>Manfaat yang saya rasakan sebagai Direktur Rumah Sakit Al-Islam Bandung dengan
+                                penggunaan SIMRS NCI-MediSmart diantaranya adalah kecepatan dalam pengambilan
+                                keputusan, akurasi dalam mengidentifikasi masalah, kemudahan menyusun strategi...
+                            </p>
                             <a href="/DetailBlog">Pelajari Lanjut...</a>
                             <div class="update">
                                 <div class="name">
@@ -45,44 +49,58 @@
                             </div>
                         </div>
                     </div>
-                    <div class="bottom-card">
-                        <div class="wrapper">
-                            <div class="card">
-                                <img src="" alt="">
-                                <div class="description">
-                                    <h3>Optimalkan Pelayanan, RS Suaka Insan Terapkan Pendaftaran Pasien Secara Online
-                                    </h3>
-                                    <p>Pelayanan yang kurang optimal dan masa tunggu yang panjang, dapat menjadikan
-                                        citra rumah sakit kurang baik dalam masyarakat. Untungnya, saat ini sudah banyak
-                                        teknologi digital berupa aplikasi pelayanan kesehatan yang bisa digunakan Rumah
-                                        Sakit...</p>
-                                </div>
-                                <a href="#">Pelajari Lanjut</a>
-                            </div>
+                </div>
 
-                            <div class="card">
-                                <img src="" alt="">
-                                <div class="description">
-                                    <h3>Uji Coba Implementasi Rekam Medis Elektronik di RS Bolaang</h3>
-                                    <p>Pada hari Senin (03/07/2023), RS Bolaang telah menyelesaikan periode uji coba
-                                        implementasi Rekam Medis Elektronik dengan baik. Inisiasi uji coba ini dilatar
-                                        belakangi dengan adanya peraturan pemerintah yang mewajibkan pemakaian Rekam
-                                        Medis Elektronik yang..</p>
-                                </div>
-                                <a href="#">Pelajari Lanjut</a>
+                <div class="bottom-card">
+                    <div class="wrapper">
+                        <div class="card">
+                            <div class="image">
+                                <img src="{{ asset('assets/img/blog1.png') }}" alt="">
                             </div>
+                            <div class="description">
+                                <h3>Optimalkan Pelayanan, RS Suaka Insan Terapkan Pendaftaran Pasien Secara Online
+                                </h3>
+                                <p>Pelayanan yang kurang optimal dan masa tunggu yang panjang, dapat menjadikan
+                                    citra rumah sakit kurang baik dalam masyarakat. Untungnya, saat ini sudah banyak
+                                    teknologi digital berupa aplikasi pelayanan kesehatan yang bisa digunakan Rumah
+                                    Sakit...
+                                </p>
+                            </div>
+                            <a href="#">Pelajari Lanjut</a>
+                        </div>
 
-                            <div class="card">
-                                <img src="" alt="">
-                                <div class="description">
-                                    <h3>Mengenal Orthorexia Nervosa, Obsesi Terhadap Makanan Sehat</h3>
-                                    <p>Makanan sehat adalah hal yang penting untuk menjaga kesehatan tubuh. Karena itu
-                                        mengkonsumsi makanan sehat adalah keharusan untuk setiap orang. Namun keinginan
-                                        kuat yang berujung menjadi obsesi terhadap makanan sehat justru mengindikasikan
-                                        suatu kelainan....</p>
-                                </div>
-                                <a href="#">Pelajari Lanjut</a>
+                        <div class="card">
+                            <div class="image">
+                                <img src="{{ asset('assets/img/blog2.png') }}" alt="">
                             </div>
+                            <div class="description">
+                                <h3>Uji Coba Implementasi Rekam Medis Elektronik di RS Bolaang
+                                </h3>
+                                <p>
+                                    Pada hari Senin (03/07/2023), RS Bolaang telah menyelesaikan periode uji coba
+                                    implementasi Rekam Medis Elektronik dengan baik. Inisiasi uji coba ini dilatar
+                                    belakangi dengan adanya peraturan pemerintah yang mewajibkan pemakaian Rekam Medis
+                                    Elektronik yang..
+                                </p>
+                            </div>
+                            <a href="#">Pelajari Lanjut</a>
+                        </div>
+
+                        <div class="card">
+                            <div class="image">
+                                <img src="{{ asset('assets/img/blog3.png') }}" alt="">
+                            </div>
+                            <div class="description">
+                                <h3>Mengenal Orthorexia Nervosa, Obsesi Terhadap Makanan Sehat
+                                </h3>
+                                <p>
+                                    Makanan sehat adalah hal yang penting untuk menjaga kesehatan tubuh. Karena itu
+                                    mengkonsumsi makanan sehat adalah keharusan untuk setiap orang. Namun keinginan kuat
+                                    yang berujung menjadi obsesi terhadap makanan sehat justru mengindikasikan suatu
+                                    kelainan....
+                                </p>
+                            </div>
+                            <a href="#">Pelajari Lanjut</a>
                         </div>
                     </div>
                 </div>
@@ -148,8 +166,10 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="button">
-                    <a href="#" class="btn-load">load More</a>
+                    <div class="btn-load">load More</div>
+                    <div class="btn-less">load Less</div>
                 </div>
             </div>
         </div>
@@ -165,5 +185,44 @@
 <script src="{{ asset('js/swiper-bundle.min.js') }}"></script>
 
 <script src="{{ asset('js/Script.js') }}"></script>
+
+{{-- Load More --}}
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        let loadMoreBtn = document.querySelector('.btn-load');
+        let loadLessBtn = document.querySelector('.btn-less');
+        let currentItem = 6;
+
+        loadMoreBtn.addEventListener('click', () => {
+            let boxes = document.querySelectorAll('.section-testimoni .content .wrapper .card');
+
+            for (var i = currentItem; i < currentItem + 3 && i < boxes.length; i++) {
+                boxes[i].style.display = 'block';
+            }
+
+            currentItem += 3;
+
+            if (currentItem >= boxes.length) {
+                loadMoreBtn.style.display = 'none';
+                // loadLessBtn.style.display = 'inline-block';
+            }
+        });
+
+        // loadLessBtn.addEventListener('click', () => {
+        //     let boxes = document.querySelectorAll('.section-testimoni .content .wrapper .card');
+
+        //     for (var i = boxes.length - 1; i >= currentItem - 3; i--) {
+        //         boxes[i].style.display = 'none';
+        //     }
+
+        //     currentItem -= 3;
+
+        //     if (currentItem <= 3) {
+        //         loadMoreBtn.style.display = 'inline-block';
+        //         loadLessBtn.style.display = 'none';
+        //     }
+        // });
+    });
+</script>
 
 </html>
