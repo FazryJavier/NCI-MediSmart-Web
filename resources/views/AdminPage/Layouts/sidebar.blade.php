e352aace068233451abfbabcd34efda04238e0d5
Fazry Javier Nugraha <fazryjavier125@gmail.com>
2023-09-03 17:05:29 +0700

    add sidebar option

--- resources/views/AdminPage/Layouts/sidebar.blade.php
+++ resources/views/AdminPage/Layouts/sidebar.blade.php
@@ -13,9 +13,9 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+                <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
                 <li class="nav-header">HALAMAN</li>
+                {{-- Landing Page --}}
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-copy"></i>
@@ -34,13 +34,25 @@
                         <li class="nav-item">
                             <a href="pages/layout/boxed.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Banner</p>
+                                <p>Client</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="pages/layout/fixed-sidebar.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Pengalaman</p>
+                                <p>Experience</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/layout/boxed.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Map</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/layout/boxed.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Product</p>
                             </a>
                         </li>
                         <li class="nav-item">
@@ -49,13 +61,22 @@
                                 <p>Video</p>
                             </a>
                         </li>
+                        <li class="nav-item">
+                            <a href="pages/layout/boxed.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>News & Update</p>
+                            </a>
+                        </li>
                     </ul>
                 </li>
+                {{-- End Landing Page --}}
+
+                {{-- Product --}}
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-chart-pie"></i>
                         <p>
-                            SIMRS
+                            Product
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
@@ -69,25 +90,25 @@
                         <li class="nav-item">
                             <a href="pages/layout/boxed.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Banner</p>
+                                <p>Video</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="pages/layout/fixed-sidebar.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Kelebihan SIMRS</p>
+                                <p>Client</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="pages/layout/fixed-topnav.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Sparasi Section 3</p>
+                                <p>Kelebihan</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="pages/layout/fixed-footer.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Modul SIMRS</p>
+                                <p>Modul</p>
                             </a>
                         </li>
                         <li class="nav-item">
@@ -96,19 +117,16 @@
                                 <p>Testimoni</p>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a href="pages/layout/collapsed-sidebar.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>CTA</p>
-                            </a>
-                        </li>
                     </ul>
                 </li>
+                {{-- End Product --}}
+
+                {{-- Healthcare Solution --}}
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-tree"></i>
                         <p>
-                            RME
+                            Healthcare Solution
                             <i class="fas fa-angle-left right"></i>
                         </p>
                     </a>
@@ -116,47 +134,26 @@
                         <li class="nav-item">
                             <a href="admin-home" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Header</p>
+                                <p>Video</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="pages/layout/boxed.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Banner</p>
+                                <p>Description</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="pages/layout/fixed-sidebar.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Kelebihan SIMRS</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/layout/fixed-topnav.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Sparasi Section 3</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/layout/fixed-footer.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Modul SIMRS</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/layout/collapsed-sidebar.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Testimoni</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/layout/collapsed-sidebar.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>CTA</p>
+                                <p>Visi Misi</p>
                             </a>
                         </li>
                     </ul>
                 </li>
+                {{-- End Healthcare Solution --}}
+
+                {{-- Detail Modul --}}
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-edit"></i>
@@ -190,14 +187,11 @@
                                 <p>Testimoni</p>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a href="pages/layout/fixed-footer.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>CTA</p>
-                            </a>
-                        </li>
                     </ul>
                 </li>
+                {{-- End Detail Modul --}}
+
+                {{-- Testimoni --}}
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-table"></i>
@@ -210,29 +204,20 @@
                         <li class="nav-item">
                             <a href="admin-home" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Header</p>
+                                <p>Testimoni</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="pages/layout/boxed.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Peta</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/layout/fixed-sidebar.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>CTA</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/layout/fixed-topnav.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Testimoni</p>
+                                <p>Map</p>
                             </a>
                         </li>
                     </ul>
                 </li>
+                {{-- End Testimoni --}}
+
+                {{-- Blog --}}
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-book"></i>
@@ -255,12 +240,6 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/layout/fixed-sidebar.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>CTA</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
                             <a href="pages/layout/fixed-topnav.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>News</p>
@@ -268,6 +247,9 @@
                         </li>
                     </ul>
                 </li>
+                {{-- End Blog --}}
+
+                {{-- Detail Blog --}}
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon far fa-plus-square"></i>
@@ -284,12 +266,6 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/layout/boxed.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>CTA</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
                             <a href="pages/layout/fixed-sidebar.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Rekomendasi</p>
@@ -297,6 +273,79 @@
                         </li>
                     </ul>
                 </li>
+                {{-- End Detail Blog --}}
+
+                {{-- Header --}}
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon far fa-plus-square"></i>
+                        <p>
+                            Navbar
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="admin-home" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Top Navbar</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/layout/fixed-sidebar.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Bottom Bar</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                {{-- End Header --}}
+
+                {{-- Footer --}}
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon far fa-plus-square"></i>
+                        <p>
+                            Footer
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="admin-home" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Footer</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/layout/fixed-sidebar.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Copyright</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                {{-- End Footer --}}
+
+                {{-- CTA --}}
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon far fa-plus-square"></i>
+                        <p>
+                            CTA
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="admin-home" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>CTA</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                {{-- End CTA --}}
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
