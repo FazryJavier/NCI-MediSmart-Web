a271e6e9c589de70d4cb2e2272c272ce0245dbbc
Fazry Javier Nugraha <fazryjavier125@gmail.com>
2023-09-27 16:51:52 +0700

    add navbar and footer database

---
+++ app/Http/Controllers/NavbarController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Navbar;
+use Illuminate\Http\Request;
+
+class NavbarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Navbar $navbar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Navbar $navbar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Navbar $navbar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Navbar $navbar)
+    {
+        //
+    }
+}
